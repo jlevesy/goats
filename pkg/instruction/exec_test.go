e6a3f6ce8c659cd4ab6a04c0eb6e1b78766a963d
Julien Levesy <jlevesy@gmail.com>
2019-10-31 03:32:40 +0100

    add some tests for the exec instruction

--- pkg/instruction/exec_test.go
+++ pkg/instruction/exec_test.go
@@ -1,13 +1,70 @@
 package instruction_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/jlevesy/goats/pkg/goats"
 	"github.com/jlevesy/goats/pkg/instruction"
+	gtesting "github.com/jlevesy/goats/pkg/testing"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExecImplements(t *testing.T) {
 	assert.Implements(t, (*goats.Instruction)(nil), &instruction.Exec{})
+}
+
+func TestExec_Exec(t *testing.T) {
+	tests := []struct {
+		name         string
+		cmd          []string
+		wantStatus   gtesting.Status
+		wantStdout   []byte
+		wantExitCode int
+	}{
+		{
+			name:       "it executes the command and captures stdout",
+			cmd:        []string{"echo", "-n", "coucou"},
+			wantStatus: gtesting.StatusSuccess,
+			wantStdout: []byte("coucou"),
+		},
+		{
+			name:         "it captures the error",
+			cmd:          []string{"curl", "lalalalalalala"},
+			wantStatus:   gtesting.StatusSuccess,
+			wantStdout:   []byte{},
+			wantExitCode: 6,
+		},
+		{
+			name:       "it reports fatal on technical error",
+			cmd:        []string{"dhdhdhdh"},
+			wantStatus: gtesting.StatusFatal,
+			wantStdout: []byte{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := context.Background()
+			exec := instruction.NewExec(test.cmd)
+			gt := gtesting.NewT("test")
+
+			exec.Exec(ctx, gt)
+
+			assert.Equal(t, test.wantStatus, gt.Status())
+
+			if test.wantStatus == gtesting.StatusFatal {
+				// Should get an output here !
+				return
+			}
+
+			out, _ := instruction.GetExecOutput(gt)
+			assert.NotNil(t, out)
+			assert.Equal(t, test.wantStdout, out.Stdout)
+
+			if out.Err != nil {
+				assert.Equal(t, test.wantExitCode, out.Err.ExitCode())
+			}
+		})
+	}
 }
